a3e7c9ee0a219074992d8d8109f165105fb6b1b5
KimUS94 <dmltjq2524@gmail.com>
2021-05-03 14:43:37 +0900

    test code

---
+++ Dll_Reloader/AssemblyLoader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+namespace Dll_Reloader
+{
+    public class AssemblyLoader : MarshalByRefObject
+    {
+        private System.Reflection.Assembly m_Assembly;
+        private Type m_Type;
+        private object m_instance;
+
+        public AssemblyLoader()
+        {
+            m_Assembly = null;
+            m_Type = null;
+            m_instance = null;
+        }
+
+        ~AssemblyLoader()
+        {
+            m_Assembly = null;
+            m_Type = null;
+            m_instance = null;
+        }
+
+        public void Load(string path)
+        {
+            this.m_Assembly = System.Reflection.Assembly.Load(AssemblyName.GetAssemblyName(path));
+        }
+
+        public object ExecuteMethod(string strModule, string methodName, params object[] parameters)
+        {
+            foreach (System.Type type in this.m_Assembly.GetTypes())
+            {
+                if (String.Compare(type.Name, strModule, true) == 0)
+                {
+                    this.m_Type = type;
+                    this.m_instance = m_Assembly.CreateInstance(type.FullName);
+                    break;
+                }
+            }
+
+            //MethodInfo MyMethod = MyType.GetMethod(methodName, new Type[] { typeof(int), typeof(string), typeof(string), typeof(string) });
+            //MyMethod.Invoke(inst, BindingFlags.InvokeMethod, null, parameters, null);
+
+            MethodInfo MyMethod = this.m_Type.GetMethod(methodName);
+            object obj = MyMethod.Invoke(this.m_instance, BindingFlags.InvokeMethod, null, parameters, null);
+
+            return obj;
+        }
+    }
+}
---
+++ Dll_Reloader/MainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dll_Reloader
+{
+    public partial class MainForm : Form
+    {
+        private string m_path;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            m_path = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "ClassLibrary.dll");
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+            object result;
+
+            result = MashallLoad();
+
+            MessageBox.Show(result.ToString());
+        }
+
+        private object MashallLoad()
+        {
+            try
+            {
+                //Create 'New AppDomain' to load 'Assembly'
+                AppDomainSetup setup = AppDomain.CurrentDomain.SetupInformation;
+                AppDomain newDomain = AppDomain.CreateDomain("NewDom", AppDomain.CurrentDomain.Evidence, setup);
+
+                //Create instance of 'AssemblyLoader' class in new appdomain  
+                System.Runtime.Remoting.ObjectHandle obj = newDomain.CreateInstance(typeof(AssemblyLoader).Assembly.FullName, typeof(AssemblyLoader).FullName);
+
+                //The instance we created came from a different AppDomain
+                //We will get that instance in wrapped format
+                //So we have to unwrap it
+                AssemblyLoader loader = (AssemblyLoader)obj.Unwrap();
+
+                //Call loadassembly method 
+                //so that the assembly will be loaded into the new appdomain and the object will also remain in the new appdomain only.  
+                loader.Load(m_path);
+
+                //Call exceuteMethod and pass the name of the method from assembly and the parameters.  
+                object result = loader.ExecuteMethod("Class", "TestMethod", null);
+
+                //After the method has been executed call the unload method of the appdomain.  
+                AppDomain.Unload(newDomain);
+
+                return result;
+            }
+            catch(Exception ex)
+            {
+                //
+                return null;
+            }
+        }
+
+
+    }
+}
